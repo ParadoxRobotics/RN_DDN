2b35b739afb33980bdb16964f2e61b3bcc1ce0f6
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2019-11-14 22:53:19 +0100

    Update Dense_Embbeding_Network.py
    new contrastive loss

--- Dense_Embbeding_Network.py
+++ Dense_Embbeding_Network.py
@@ -1,3 +1,4 @@
+
 from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
@@ -118,7 +119,7 @@ print("RN_DEN STRUCTURE : \n", RN_DEN)
 
 
 class ContrastiveLoss(torch.nn.Module):
-    def __init__(self, margin=0.005, non_match_loss_weight=1.0):
+    def __init__(self, margin=0.5, non_match_loss_weight=1.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
         self.non_match_loss_weight = non_match_loss_weight
@@ -133,25 +134,26 @@ class ContrastiveLoss(torch.nn.Module):
         nb_non_match = non_match_A.size()[0]
 
         # create a tensor with the listed matched descriptors in the estimated descriptors map (net output)
-        match_A_des = torch.index_select(out_A, 1, match_A)
-        match_B_des = torch.index_select(out_B, 1, match_B)
+        match_A_des = torch.index_select(out_A, 1, match_A).unsqueeze(0)
+        match_B_des = torch.index_select(out_B, 1, match_B).unsqueeze(0)
         # create a tensor with the listed non-matched descriptors in the estimated descriptors map (net output)
-        non_match_A_des = torch.index_select(out_A, 1, non_match_A)
-        non_match_B_des = torch.index_select(out_B, 1, non_match_B)
+        non_match_A_des = torch.index_select(out_A, 1, non_match_A).unsqueeze(0)
+        non_match_B_des = torch.index_select(out_B, 1, non_match_B).unsqueeze(0)
 
         # calculate match loss (L2 distance)
-        match_loss = (1.0/nb_match) * (match_A_des - match_B_des).pow(2).sum()
-        # calculate non-match loss (pixelwise L2 distance with margin)
-        pixel_wise_loss = (non_match_A_des - non_match_B_des).pow(2).sum(dim=2)
-        pixel_wise_loss = torch.add(torch.neg(pixel_wise_loss), self.margin)
-        zeros_vec = torch.zeros_like(pixel_wise_loss)
-        non_match_loss = self.non_match_loss_weight * (1.0/nb_non_match) * torch.max(zeros_vec, pixel_wise_loss).sum()
+        match_loss = 1.0/nb_match * (match_A_des - match_B_des).pow(2).sum()
+        # calculate non-match loss (L2 distance with margin)
+        zeros_vec = torch.zeros_like(non_match_A_des)
+        pixelwise_non_match_loss = torch.max(zeros_vec, self.margin-((non_match_A_des - non_match_B_des).pow(2)))
+        # Hard negative scaling (pixelwise)
+        hard_negative_non_match = len(torch.nonzero(pixelwise_non_match_loss))
+        # final non_match loss with hard negative scaling
+        non_match_loss = self.non_match_loss_weight * 1.0/hard_negative_non_match * pixelwise_non_match_loss.sum()
 
         # final contrastive loss
         contrastive_loss = match_loss + non_match_loss
 
         return contrastive_loss, match_loss, non_match_loss
-
 
 # init cost function for training
 cost_function = ContrastiveLoss()
