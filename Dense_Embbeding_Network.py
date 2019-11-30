8f2c0b2aadff897cf05ce9a786f8c78196cd0219
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2019-11-30 19:56:17 +0100

    Update Dense_Embbeding_Network.py
    improved ResNet 34 that match what we can found in the publication.
    In "kPAM: KeyPoint Affordances forCategory-Level Robotic Manipulation" they use a ResNet34 like network with stride = 8  instead of 1 or 2.

--- Dense_Embbeding_Network.py
+++ Dense_Embbeding_Network.py
@@ -23,96 +23,81 @@ from collections import OrderedDict
 #        ResNet Pyramid Network for Dense Embedding Network (RN-DEN)
 #-------------------------------------------------------------------------------
 
-# Custom ResNet (ResNet34-like):
 class ResBlock(nn.Module):
-    def __init__(self, in_channel, out_channel, stride_size):
+    def __init__(self, inchannel, outchannel, stride=1, shortcut=None):
         super(ResBlock, self).__init__()
-        # Internal block
-        self.conv_1 = nn.Conv2d(in_channel, out_channel, 3, stride_size, 1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(out_channel)
-        self.conv_2 = nn.Conv2d(out_channel, out_channel, 3, 1, 1, bias=False)
-        self.bn_2 = nn.BatchNorm2d(out_channel)
-        # Residual connection
-        self.residual = nn.Sequential()
-        if stride_size != 1 or in_channel != out_channel:
-            self.residual = nn.Sequential(
-                nn.Conv2d(in_channel, out_channel, 3, stride_size, 1, bias=False),
-                nn.BatchNorm2d(out_channel)
-            )
+        # create residual block
+        self.block = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outchannel, outchannel, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(outchannel)
+        )
+        self.shortcut = shortcut
 
+    # Block with residual connection
     def forward(self, x):
-        out = self.conv_1(x)
-        out = self.bn_1(out)
-        out = F.relu(out)
-        out = self.conv_2(out)
-        out = self.bn_2(out)
-        out = F.relu(out)
-        out = out + self.residual(x)
-        out = F.relu(out)
-        return out
+        out = self.block(x)
+        residual = x if self.shortcut is None else self.shortcut(x)
+        out += residual
+        return F.relu(out)
 
 class RN_DEN(nn.Module):
-    def __init__(self, main_channel, block, num_block):
+    def __init__(self):
         super(RN_DEN, self).__init__()
-        # Input size for each residual block
-        self.residual_input = 16
-        # First input
-        self.conv_1 = nn.Conv2d(main_channel, 16, 3, 1, 1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(16)
-        # Residual block declaration
-        self.layer_1 = self.build_block(block, 16, num_block[0], 1)
-        self.layer_2 = self.build_block(block, 32, num_block[1], 2)
-        self.layer_3 = self.build_block(block, 64, num_block[2], 2)
-        self.layer_4 = self.build_block(block, 128, num_block[2], 2)
-        self.layer_5 = self.build_block(block, 256, num_block[2], 2)
-        self.layer_6 = self.build_block(block, 512, num_block[2], 2)
+        # input layer
+        self.first_processing = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, 3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1)
+        )
+        # Residual block layer (ResNet34 layer [3,4,6,3]
+        self.layer_1 = self.build_block(64, 128, 3)
+        self.layer_2 = self.build_block(128, 256, 4, stride=2)
+        self.layer_3 = self.build_block(256, 512, 6, stride=2)
+        self.layer_4 = self.build_block(512, 512, 3, stride=2)
         # Lateral convolutional layer
         self.lateral_layer_1 = nn.Conv2d(256, 512, kernel_size=1, stride=1, padding=0)
         self.lateral_layer_2 = nn.Conv2d(128, 512, kernel_size=1, stride=1, padding=0)
         self.lateral_layer_3 = nn.Conv2d(64, 512, kernel_size=1, stride=1, padding=0)
-        self.lateral_layer_4 = nn.Conv2d(32, 512, kernel_size=1, stride=1, padding=0)
-        self.lateral_layer_5 = nn.Conv2d(16, 512, kernel_size=1, stride=1, padding=0)
         # Bilinear Upsampling
         self.up_1 = nn.Upsample(scale_factor=2, mode='bilinear')
         self.up_2 = nn.Upsample(scale_factor=2, mode='bilinear')
         self.up_3 = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.up_4 = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.up_5 = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.up_4 = nn.Upsample(scale_factor=4, mode='bilinear')
 
-    def build_block(self, block, out_channel, num_block, stride):
-        strides = [stride] + [1]*(num_block-1)
+    def build_block(self, inchannel, outchannel, block_num, stride=1):
+        shortcut = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, 1, stride, bias=False),
+            nn.BatchNorm2d(outchannel)
+        )
         layers = []
-        for stride in strides:
-            layers.append(block(self.residual_input, out_channel, stride))
-            self.residual_input = out_channel
+        layers.append(ResBlock(inchannel, outchannel, stride, shortcut))
+        for i in range(1, block_num):
+            layers.append(ResBlock(outchannel, outchannel))
         return nn.Sequential(*layers)
 
     def forward(self, x):
         # input processing
-        out = self.conv_1(x)
-        out = self.bn_1(out)
-        out = F.relu(out)
+        out_p = self.first_processing(x)
         # residual processing (fully convolutional)
-        out_1 = self.layer_1(out)
+        out_1 = self.layer_1(out_p)
         out_2 = self.layer_2(out_1)
         out_3 = self.layer_3(out_2)
         out_4 = self.layer_4(out_3)
-        out_5 = self.layer_5(out_4)
-        out_6 = self.layer_5(out_6)
         # Upsampling processing
-        out_up_1 = self.up_1(out_6)
-        out_up_2 = self.up_2(out_up_1 + self.lateral_layer_1(out_5))
-        out_up_3 = self.up_3(out_up_2 + self.lateral_layer_2(out_4))
-        out_up_4 = self.up_4(out_up_3 + self.lateral_layer_3(out_3))
-        out_up_5 = self.up_5(out_up_4 + self.lateral_layer_4(out_2))
-        out_up_6 = out_up_5 + self.lateral_layer_5(out_1)
-        # return hidden + output
+        out_up_1 = self.up_1(out_4) + out_3
+        out_up_2 = self.up_2(out_up_1) + self.lateral_layer_1(out_2)
+        out_up_3 = self.up_3(out_up_2) + self.lateral_layer_2(out_1)
+        out_up_4 = self.up_4(out_up_3 + self.lateral_layer_3(out_p))
         return out_4, out_up_4
 
-
 # Network instantiation and test
-RN_DEN = RN_DEN(3, ResBlock, [5,5,5,5,5,5])
+RN_DEN = RN_DEN()
 print("RN_DEN STRUCTURE : \n", RN_DEN)
+
 
 #-------------------------------------------------------------------------------
 #                        Dense Contrastive Loss
@@ -125,7 +110,7 @@ class ContrastiveLoss(torch.nn.Module):
         self.margin = margin
         self.non_match_loss_weight = non_match_loss_weight
 
-    def forward(self, out_A, Oout_B, match_A, match_B, non_match_A, non_match_B):
+    def forward(self, out_A, out_B, match_A, match_B, non_match_A, non_match_B):
         # out_A, out_B : [1, D, H, W] => [1, H*W, D]
         # match_A, match_B, non_match_A, non_match_B : [nb_match,] with (u,v)
         # (u,v) -> image_width * v + u
@@ -175,8 +160,8 @@ class Generate_Correspondence(torch.nn.Module):
 
     def RGBD_matching(self, in_A, depth_A, pose_A, in_B, depth_B, pose_B):
         # Image and depth map need to aligned :
-        #  - in_A/in_B -> [H,W,C]
-        #  - depth_A/depth_B -> [H,W]
+        #  - in_A/in_B -> [1, D, H, W] -> [0, c, v, u]
+        #  - depth_A/depth_B -> [1, H, W] -> [0, v, u]
 
         # Init match list
         valid_match_A = []
@@ -197,14 +182,14 @@ class Generate_Correspondence(torch.nn.Module):
         uv_A = torch.zeros(2).type(torch.IntTensor)
         uv_B = torch.zeros(2).type(torch.IntTensor)
 
-        for i in range(0,self.number_match):
+        while (len(valid_match_A) != self.number_match):
             # Generate random point in the [uA,vA] image space
-            uv_A[0] = randint(0, in_A.size(0)-1)
-            uv_A[1] = randint(0, in_A.size(1)-1)
+            uv_A[0] = randint(0, in_A.size(3)-1) # Width = u
+            uv_A[1] = randint(0, in_A.size(2)-1) # Height = v
             # Evaluate depth (DA>0)
-            if depth_A[uv_A[0], uv_A[1]] > 0:
+            if depth_A[0, uv_A[1], uv_A[0]] > 0:
                 # Generate [xA,yA,zA] points (camera parameters + depth)
-                Pt_A[2,0] = depth_A[uv_A[0], uv_A[1]]/self.depth_scale
+                Pt_A[2,0] = depth_A[0, uv_A[1], uv_A[0]]/self.depth_scale
                 Pt_A[0,0] = (uv_A[0]-self.intrinsic_mat[0,2])*(Pt_A[2,0]/self.intrinsic_mat[0,0])
                 Pt_A[1,0] = (uv_A[1]-self.intrinsic_mat[1,2])*(Pt_A[2,0]/self.intrinsic_mat[1,1])
             else:
@@ -217,20 +202,20 @@ class Generate_Correspondence(torch.nn.Module):
             uv_B[0] =((self.intrinsic_mat[0,0]*Pt_B[0,0])/Pt_B[2,0])+self.intrinsic_mat[0,2]
             uv_B[1] =((self.intrinsic_mat[1,1]*Pt_B[1,0])/Pt_B[2,0])+self.intrinsic_mat[1,2]
             # Evaluate frustum consistency, depth DB > 0 and occlusion
-            if (uv_B[0]<in_B.size(0)) and (uv_B[0]>0) and (uv_B[1]<in_B.size(1)) and (uv_B[1]>0) and (depth_B[uv_B[0],uv_B[1]]>0) and depth_B[uv_B[0], uv_B[1]] >= Pt_B[2,0]-depth_margin:
+            if (uv_B[0]<in_B.size(3)) and (uv_B[0]>0) and (uv_B[1]<in_B.size(2)) and (uv_B[1]>0) and (depth_B[0, uv_A[1], uv_A[0]]>0) and (depth_B[0, uv_A[1], uv_A[0]] >= Pt_B[2,0]-depth_margin):
                 # store good match in list
                 valid_match_A.append(copy.deepcopy(uv_A))
                 valid_match_B.append(copy.deepcopy(uv_B))
             else:
                 continue
-        # return all match in image A and image B
+        # return all match in image A and image B with shape [u,v]
         return valid_match_A, valid_match_B
 
 
     def RGBD_non_match(self, valid_match_A, valid_match_B):
         # Image and depth map need to aligned :
-        #  - in_A / in_B -> [H,W,C]
-        #  - depth_A / depth_B -> [H,W]
+        #  - in_A/in_B -> [1, D, H, W] -> [0, c, v, u]
+        #  - depth_A/depth_B -> [1, H, W] -> [0, v, u]
 
         # Init non-match list
         non_valid_match_A = []
@@ -246,7 +231,6 @@ class Generate_Correspondence(torch.nn.Module):
 
         # return all non-match in image A and image B
         return non_valid_match_A, non_valid_match_B
-
 
 # correspondence generator parameter init
 # Camera intrinsic parameters
@@ -272,7 +256,7 @@ correspondence_generator = Generate_Correspondence(intrinsic_mat, depth_scale, d
 #-------------------------------------------------------------------------------
 
 # SGD optimizer with Nesterov momentum
-optimizer = optim.SGD(RN.parameters(), lr = 0.01,
+optimizer = optim.SGD(RN_DEN.parameters(), lr = 0.01,
                                             momentum = 0.90,
                                             weight_decay = 0.00001,
                                             nesterov = True)
@@ -283,6 +267,8 @@ number_epoch = 10
 lrPower = 2
 lambda1 = lambda epoch: (1.0 - epoch / number_epoch) ** lrPower
 scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=[lambda1])
+
+
 
 #-------------------------------------------------------------------------------
 #                            Learning procedure
