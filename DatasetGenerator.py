5d328c1c2a01a82be989c63e64745d92686f19cc
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2022-01-26 07:52:33 +0100

    Add files via upload

--- DatasetGenerator.py
+++ DatasetGenerator.py
@@ -7,10 +7,12 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-FileToStore = "/home/neurotronics/Bureau/DDN/dataset/sequence"
-VideoFile = "/home/neurotronics/Bureau/DDN/dataset/test.mp4"
+FileToStoreImgA = "/home/neurotronics/Bureau/DDN/dataset/ImgA"
+FileToStoreImgB = "/home/neurotronics/Bureau/DDN/dataset/ImgB"
+VideoFile = "/home/neurotronics/Bureau/DDN/dataset/seq1.mp4"
 sampling = 15
 idx = 0
+step = 0
 # load video file
 cap = cv2.VideoCapture(VideoFile)
 counter = 0
@@ -21,7 +23,14 @@ while(cap.isOpened()):
         break
     if counter % sampling == 0:
         # store frame
-        print(FileToStore+'/'+str(idx)+'.png')
-        cv2.imwrite(FileToStore+"/"+str(int(idx))+".png", cv2.resize(frame, (640,480)))
-        idx+=1
+        if step % 2 == 0:
+            # Match A
+            print("Img A", FileToStoreImgA+'/'+str(idx)+'.png')
+            cv2.imwrite(FileToStoreImgA+"/"+str(int(idx))+".png", cv2.resize(frame, (640,480)))
+        else:
+            # Match B
+            print("Img B", FileToStoreImgB+'/'+str(idx)+'.png')
+            cv2.imwrite(FileToStoreImgB+"/"+str(int(idx))+".png", cv2.resize(frame, (640,480)))
+            idx+=1
+        step+=1
     counter+=1
