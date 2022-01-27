33cb2f8d5f2aa55d65db5bfcdc7d59868c8206dc
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2022-01-27 12:00:41 +0100

    Update CustomDataloader.py

--- CustomDataloader.py
+++ CustomDataloader.py
@@ -18,10 +18,11 @@ from collections import OrderedDict
 
 # Custom dataloader
 class ImagePairDataset(data.Dataset):
-    def __init__(self, ImgADir, ImgBDir, transform=None):
+    def __init__(self, ImgADir, ImgBDir, Augmentation=None):
         self.imgADir = ImgADir
         self.imgBDir = ImgBDir
-        self.transform = transform
+        self.augmentation = Augmentation
+        self.normalization = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     # Overloaded len method
     def __len__(self):
         return len(os.listdir(self.imgADir))
@@ -31,26 +32,28 @@ class ImagePairDataset(data.Dataset):
         imgAPath = os.path.join(self.imgADir, os.listdir(self.imgADir)[idx])
         imgBPath = os.path.join(self.imgBDir, os.listdir(self.imgBDir)[idx])
         # convert to color tensor with shape [B,C,H,W]
-        imgA = read_image(imgAPath)
-        imgB = read_image(imgBPath)
+        imgAMatch = read_image(imgAPath)/255
+        imgBMatch = read_image(imgBPath)/255
+        # Normalize image for training
+        imgA = self.normalization(imgAMatch)
+        imgB = self.normalization(imgBMatch)
         # Transform image B if needed
-        if self.transform != None:
-            imgB = self.transform(imgB)
-        pair = {'image A': imgA, 'image B': imgB}
+        if self.augmentation != None:
+            imgB = self.augmentation(imgB)
+        pair = {'image A': imgA, 'image B': imgB, 'image A Match': imgAMatch, 'image B Match': imgBMatch}
         return pair
-"""
+
 # Load dataset
-imgPairDataset = ImagePairDataset(ImgADir="/home/main/Bureau/dataset/ImgA", ImgBDir="/home/main/Bureau/dataset/ImgB", transform=None)
+imgPairDataset = ImagePairDataset(ImgADir="/home/main/Bureau/dataset/ImgA", ImgBDir="/home/main/Bureau/dataset/ImgB", Augmentation=None)
 
 # show some data
 for idx in range(len(imgPairDataset)):
     sample = imgPairDataset[idx]
-    print(sample['image A'].size(), sample['image B'].size())
+    print(sample['image A Match'].size(), sample['image B Match'].size())
     # plot figure
     fig = plt.figure()
     fig.add_subplot(1, 2, 1)
-    plt.imshow(sample['image A'].permute(1,2,0).detach().numpy())
+    plt.imshow(sample['image A Match'].permute(1,2,0).detach().numpy())
     fig.add_subplot(1, 2, 2)
-    plt.imshow(sample['image B'].permute(1,2,0).detach().numpy())
+    plt.imshow(sample['image B Match'].permute(1,2,0).detach().numpy())
     plt.show()
-"""
