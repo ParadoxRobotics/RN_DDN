63431d2bc3bb8a9c40e4f17afd3d76b5c8a90a2e
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2022-02-02 21:57:00 +0100

    Add files via upload
    almost ready
    TODO:
    - check dataloading with cuda 
    - check keypoint extraction using cuda and the current image/type/shape 
    - test inference with 2 networks on the same GPU (memory crash ?) 
    - finish testing pipeline
    - Find Meta-parameters

--- DenseDescriptorNetwork.py
+++ DenseDescriptorNetwork.py
@@ -7,68 +7,135 @@
 # Dense Descriptor Network for object detection, manipulation and navigation.
 # Author : Munch Quentin, 2022.
 
+# General and computer vision lib
 import math
 from random import randint
+import copy
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
+# Neural network Torch lib
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import models
-from torchvision import transforms
-
+from torch.optim import lr_scheduler
+import torch.utils.data as data
+from torchvision import models, datasets, transforms, utils
+# Kornia computer vision differential lib
 import kornia as K
 import kornia.feature as KF
 
-# Contrastive Loss function
-class ContrastiveLoss(torch.nn.Module):
-    def __init__(self, margin=0.5, nonMatchLossWeight=1.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-        self.nonMatchLossWeight = nonMatchLossWeight
-    # Loss estimation with hard negative mining
-    def forward(self, outA, outB, matchA, matchB, nonMatchA, nonMatchB):
-        # outA, outB : [B, D, H, W] => [B, H*W, D]
-        # matchA, matchB, nonMatchA, nonMatchB : [B, nb_match] with (u,v)
-        # (u,v) -> image_width * v + u
-        # init loss
-        contrastiveLossSum = 0
-        matchLossSum = 0
-        nonMatchLossSum = 0
-        # reshape Network output (A and B)
-        # for every element in the batch
-        for b in range(0,outA.size()[0]):
-            # get the number of match/non-match (tensor float)
-            nbMatch = len(matchA[b])
-            nbNonMatch = len(nonMatchA[b])
-            # create a tensor with the listed matched descriptors in the estimated descriptors map (net output)
-            matchADes = torch.index_select(outA[b].unsqueeze(0), 1, matchA[b]).unsqueeze(0)
-            matchBDes = torch.index_select(outB[b].unsqueeze(0), 1, matchB[b]).unsqueeze(0)
-            # create a tensor with the listed non-matched descriptors in the estimated descriptors map (net output)
-            nonMatchADes = torch.index_select(outA[b].unsqueeze(0), 1, nonMatchA[b]).unsqueeze(0)
-            nonMatchBDes = torch.index_select(outB[b].unsqueeze(0), 1, nonMatchB[b]).unsqueeze(0)
-            # calculate match loss (L2 distance)
-            matchLoss = 1.0/nbMatch * (matchADes - matchBDes).pow(2).sum()
-            # calculate non-match loss (L2 distance with margin)
-            zerosVec = torch.zeros_like(nonMatchADes)
-            pixelwiseNonMatchLoss = torch.max(zerosVec, self.margin-((nonMatchADes - nonMatchBDes).pow(2)))
-            # Hard negative scaling (pixelwise)
-            hardNegativeNonMatch = len(torch.nonzero(pixelwiseNonMatchLoss))
-            # final non_match loss with hard negative scaling
-            nonMatchloss = self.nonMatchLossWeight * 1.0/hardNegativeNonMatch * pixelwiseNonMatchLoss.sum()
-            # compute contrastive loss
-            contrastiveLoss = matchLoss + nonMatchloss
-            # update final losses
-            contrastiveLossSum += contrastiveLoss
-            matchLossSum += matchLoss
-            nonMatchLossSum += nonMatchloss
+# Image pair dataloader
+class ImagePairDataset(data.Dataset):
+    def __init__(self, ImgADir, ImgBDir, Augmentation):
+        self.imgADir = ImgADir
+        self.imgBDir = ImgBDir
+        self.augmentation = Augmentation
+        self.normalization = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.colorJitter = transforms.ColorJitter(brightness=.5, hue=.3)
+    # Overloaded len method
+    def __len__(self):
+        return len(os.listdir(self.imgADir))
+    # Overloaded getter method
+    def __getitem__(self, idx):
+        # Load specific image with index
+        imgAPath = os.path.join(self.imgADir, os.listdir(self.imgADir)[idx])
+        imgBPath = os.path.join(self.imgBDir, os.listdir(self.imgBDir)[idx])
+        # convert to color tensor with shape [B,C,H,W]
+        imgAMatch = read_image(imgAPath)
+        imgBMatch = read_image(imgBPath)
+        imgA = imgAMatch.clone()
+        imgB = imgBMatch.clone()
+        # Data augmentation for the image B (match and training)
+        if self.augmentation == True:
+            # Random H flip
+            if random.random() > 0.5:
+                imgBMatch = transforms.functional.hflip(imgBMatch)
+                imgB = transforms.functional.hflip(imgB)
+            # Random V flip
+            if random.random() > 0.5:
+                imgBMatch = transforms.functional.vflip(imgBMatch)
+                imgB = transforms.functional.vflip(imgB)
+            # Random ColorJitter
+            if random.random() > 0.5:
+                imgB = self.colorJitter(imgB)
+        # Normalize image for training and matching
+        imgAMatch = imgAMatch/255.
+        imgBMatch = imgBMatch/255.
+        imgA = self.normalization(imgA/255.)
+        imgB = self.normalization(imgB/255.)
+        # create a dictionnary for access
+        pair = {'image A': imgA, 'image B': imgB, 'image A Match': imgAMatch, 'image B Match': imgBMatch}
+        return pair
 
-        return contrastiveLossSum, matchLossSum, nonMatchLossSum
-
+# Find correspondences between 2 images and generate non-matches from it
+def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch=150, device):
+    # ----------------------------------------------------------------------------------
+    # INPUT :
+    # - The matcher must be LoFTR type neural network
+    # - ImgA and ImgB must be a RGB/255 tensor with shape [B,C,H,W]
+    # - NumberNonMatchPerMatch is the number of non-match in imageB for each good
+    # match in A that need to be generated
+    # - Device use for the inference
+    # OUPUT :
+    # - matchA / matchB / nonMatchA / nonMatchB tensor with shape [B, nb_match]
+    # match/non-match = image_width * row + column
+    # ---------------------------------------------------------------------------------
+    # Get batch size
+    batchSize = ImgA.size()[0]
+    H = ImgA.size()[2]
+    W = ImgA.size()[3]
+    # Create a dict for the 2 images in grayscale
+    inputDict = {"image0": K.color.rgb_to_grayscale(imgA).to(device),
+                 "image1": K.color.rgb_to_grayscale(imgB).to(device)}
+    # Find correspondences using the LoFTR network
+    with torch.no_grad():
+        correspondences = matcher(inputDict)
+    # get keypoints and batch indexes
+    kp_A = correspondences['keypoints0'].cpu().numpy()
+    kp_B = correspondences['keypoints1'].cpu().numpy()
+    batchIndexKeyoints = correspondences['batch_indexes'].cpu().numpy()
+    print(kp_A.shape()[0],"Correspondences found using LoFTR in this batch")
+    # create empty list
+    matchA = []
+    matchB = []
+    nonMatchA = []
+    nonMatchB = []
+    # create matchA/matchB and non-matchA/non-matchB
+    for batch in range(0, batchSize):
+        currentBatchA = []
+        currentBatchB = []
+        currentBatchNA = []
+        currentBatchNB = []
+        # matchA / matchB are extract from keypoints at a specific batch
+        for i in range(batchIndexKeyoints.shape()[0]):
+            if batchIndexKeyoints[i] == batch:
+                currentBatchA.append(W * int(kp_A[i,0]) + int(kp_A[i,1]))
+                currentBatchB.append(W * int(kp_B[i,0]) + int(kp_B[i,1]))
+            else:
+                break
+        # update global match list
+        matchA.append(currentBatchA)
+        matchB.append(currentBatchB)
+        # non-matchA / non-matchB are generate from matchB for every keypoints
+        # in matchA
+        for i in range(len(currentBatchA)):
+            sample = 0
+            while (sample != NumberNonMatchPerMatch):
+                rdUVW = random.randint(0, (W*H))
+                if rdUVW != currentBatchB[i]:
+                    currentBatchNA.append(currentBatchA[i])
+                    currentBatchNB.append(rdUVW)
+                    sample += 1
+                else:
+                    continue
+        # update global non-match list
+        nonMatchA.append(currentBatchNA)
+        nonMatchB.append(currentBatchNB)
+    # return the batched match/non-match
+    return matchA, matchB, nonMatchA, nonMatchB
 
 # ResNet34 + FPN dense descriptor architecture
 class VisualDescriptorNet(torch.nn.Module):
@@ -87,7 +154,7 @@ class VisualDescriptorNet(torch.nn.Module):
         self.upConv32 = nn.Conv2d(512, self.descriptorDim, kernel_size=1)
         # actiavtion function for the last layer (decoder)
         self.activation = nn.ReLU()
-    # Forward pass
+    # Single Network Forward pass
     def forward(self, x):
         # get input size -> for the upsampling
         InputSize = x.size()[2:]
@@ -114,18 +181,118 @@ class VisualDescriptorNet(torch.nn.Module):
         up1 += nn.functional.interpolate(up2, size=up1Size)
         finalUp = nn.functional.interpolate(up1, size=InputSize)
         out = self.activation(finalUp)
-        return out # output of the net
+        return out
 
-# Init DDN Network, Adam optimizer and loss function
-DDN = VisualDescriptorNet(descriptorDim=16)
+# Contrastive loss function with hard-negative mining
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=0.5, nonMatchLossWeight=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.nonMatchLossWeight = nonMatchLossWeight
+    def forward(self, outA, outB, matchA, matchB, nonMatchA, nonMatchB):
+        # ----------------------------------------------------------------------------------
+        # INPUT :
+        # - Network output tensor outA and outB with the shape [B,H*W,C]
+        # - MatchA/MatchB and nonMatchA/nonMatchB with shape [B,NbMatch]
+        # Each Match/non-Match keypoint as been vectorize [x,y]->W*x+y
+        # OUPUT :
+        # - Loss sum from matching loss and non-match loss
+        # ---------------------------------------------------------------------------------
+        contrastiveLossSum = 0
+        matchLossSum = 0
+        nonMatchLossSum = 0
+        # for every element in the batch
+        for b in range(0,outA.size()[0]):
+            # get the number of match/non-match (tensor float)
+            nbMatch = len(matchA[b])
+            nbNonMatch = len(nonMatchA[b])
+            # create a tensor with the listed matched descriptors in the estimated descriptors map (net output)
+            matchADes = torch.index_select(outA[b].unsqueeze(0), 1, matchA[b]).unsqueeze(0)
+            matchBDes = torch.index_select(outB[b].unsqueeze(0), 1, matchB[b]).unsqueeze(0)
+            # create a tensor with the listed non-matched descriptors in the estimated descriptors map (net output)
+            nonMatchADes = torch.index_select(outA[b].unsqueeze(0), 1, nonMatchA[b]).unsqueeze(0)
+            nonMatchBDes = torch.index_select(outB[b].unsqueeze(0), 1, nonMatchB[b]).unsqueeze(0)
+            # calculate match loss (L2 distance)
+            matchLoss = 1.0/nbMatch * (matchADes - matchBDes).pow(2).sum()
+            # calculate non-match loss (L2 distance with margin)
+            zerosVec = torch.zeros_like(nonMatchADes)
+            pixelwiseNonMatchLoss = torch.max(zerosVec, self.margin-((nonMatchADes - nonMatchBDes).pow(2)))
+            # Hard negative scaling (pixelwise)
+            hardNegativeNonMatch = len(torch.nonzero(pixelwiseNonMatchLoss))
+            # final non_match loss with hard negative scaling
+            nonMatchloss = self.nonMatchLossWeight * 1.0/hardNegativeNonMatch * pixelwiseNonMatchLoss.sum()
+            # compute contrastive loss
+            contrastiveLoss = matchLoss + nonMatchloss
+            # update final losses
+            contrastiveLossSum += contrastiveLoss
+            matchLossSum += matchLoss
+            nonMatchLossSum += nonMatchloss
+        # return global loss, matching loss and non-match loss
+        return contrastiveLossSum, matchLossSum, nonMatchLossSum
+
+# Set the device when the the network and the matcher run
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Init DDN Network, Adam optimizer, scheduler and loss function
+descriptorSize = 16
+batchSize = 10
+nbEpoch = 50
+DDN = VisualDescriptorNet(descriptorDim=descriptorSize).to(device)
 optimizer = optim.Adam(DDN.parameters(), lr=1.0e-4, weight_decay=1.0e-4)
-lrSteps = 250
-lrDecay = 0.9
+scheduler = lr_scheduler.StepLR(optimizer, step_size=nbEpoch/2, gamma=0.9)
+contrastiveLoss = ContrastiveLoss(margin=0.5, nonMatchLossWeight=1.0)
 # Init LoFTR network
-matcher = KF.LoFTR(pretrained='indoor')
+matcher = KF.LoFTR(pretrained='indoor').to(device)
+# Load Training Dataset
+imgAFolderTraining = "/home/neurotronics/Bureau/DDN/dataset/training/ImgA"
+imgBFolderTraining = "/home/neurotronics/Bureau/DDN/dataset/training/ImgB"
+trainingDataset = ImagePairDataset(ImgADir=imgAFolderTraining, ImgBDir=imgBFolderTraining, Augmentation=True)
+# Load Testing Dataset
+imgAFolderTesting = "/home/neurotronics/Bureau/DDN/dataset/testing/ImgA"
+imgBFolderTesting = "/home/neurotronics/Bureau/DDN/dataset/testing/ImgB"
+testingDataset = ImagePairDataset(ImgADir=imgAFolderTesting, ImgBDir=imgBFolderTesting, Augmentation=False)
+# Init dataloader for training and testing
+trainingLoader = data.DataLoader(trainingDataset, batch_size=batchSize, shuffle=False, num_workers=4)
+testingLoader = data.DataLoader(testingDataset, batch_size=1, shuffle=False, num_workers=4)
 
-# load dataset
-
-# training function
-
-# testing function
+# training / testing
+for epoch in range(0,nbEpoch):
+    # Set network to trainin mode
+    DDN.train()
+    # Training on the dataset
+    for data in trainingLoader:
+        # Compute match/non-match
+        inputBatchACorr = data['image A Match'].to(device)
+        inputBatchBCorr = data['image B Match'].to(device)
+        inputBatchA = data['image A'].to(device)
+        inputBatchB = data['image B'].to(device)
+        matchA, matchB, nonMatchA, nonMatchB = CorrespondenceGenerator(Matcher=matcher,
+                                                                       ImgA=inputBatchACorr,
+                                                                       ImgB=inputBatchBCorr,
+                                                                       NumberNonMatchPerMatch=150,
+                                                                       device=device)
+        # Perform inference using the DDN
+        desA = DDN(inputBatchA)
+        desB = DDN(inputBatchB)
+        # Reshape descriptor to [Batch, H*W, Channel]
+        vectorDesA = desA.view(desA.size()[0], desA.size()[1], desA.size()[2] * desA.size()[3])
+        vectorDesA = vectorDesA.permute(0, 2, 1)
+        vectorDesB = desB.view(desB.size()[0], desB.size()[1], desB.size()[2] * desB.size()[3])
+        vectorDesB = vectorDesB.permute(0, 2, 1)
+        # Convert list to tensor with shape [Batch, nbKeypoints]
+        vectorMatchA = torch.FloatTensor(matchA)
+        vectorMatchB = torch.FloatTensor(matchB)
+        vectorNonMatchA = torch.FloatTensor(nonMatchA)
+        vectorNonMatchB = torch.FloatTensor(nonMatchB)
+        # Compute loss (update cumulated loss)
+        loss, MLoss, MNLoss = contrastiveLoss(outA=vectorDesA,
+                                              outB=vectorDesB,
+                                              matchA=vectorMatchA,
+                                              matchB=vectorMatchB,
+                                              nonMatchA=vectorNonMatchA,
+                                              nonMatchB=vectorNonMatchB)
+        # Backpropagate loss
+        loss.backward()
+        # Update weight and scheduler step
+        optimizer.step()
+    # Update scheduler
+    scheduler.step()
