3ffeb8951192756d7cc5bd4bd4b91692eb42a0dc
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2022-02-28 19:54:50 +0100

    Update LoFTRMatchingDataset.py
    new non match generation -> fuckin slow....
    TODO -> optimize

--- LoFTRMatchingDataset.py
+++ LoFTRMatchingDataset.py
@@ -64,7 +64,7 @@ class ImagePairDataset(data.Dataset):
         return pair
 
 # Find correspondences between 2 images and generate non-matches from it
-def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch, device):
+def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch):
     # ----------------------------------------------------------------------------------
     # INPUT :
     # - The matcher must be LoFTR type neural network
@@ -75,13 +75,15 @@ def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch, device)
     # - matchA / matchB / nonMatchA / nonMatchB tensor with shape [B, nb_match]
     # match/non-match = image_width * row + column
     # ---------------------------------------------------------------------------------
+    # Non match distance threshold
+    nonMatchTh = 1.0
     # Get batch size
     batchSize = ImgA.size()[0]
     H = ImgA.size()[2]
     W = ImgA.size()[3]
     # Create a dict for the 2 images in grayscale
-    inputDict = {"image0": K.color.rgb_to_grayscale(ImgA).to(device),
-                 "image1": K.color.rgb_to_grayscale(ImgB).to(device)}
+    inputDict = {"image0": K.color.rgb_to_grayscale(ImgA),
+                 "image1": K.color.rgb_to_grayscale(ImgB)}
     # Find correspondences using the LoFTR network
     with torch.no_grad():
         correspondences = Matcher(inputDict)
@@ -97,17 +99,25 @@ def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch, device)
     nonMatchB = []
     # create matchA/matchB and non-matchA/non-matchB
     for batch in range(0, batchSize):
+        # current batch list (linear)
         currentBatchA = []
         currentBatchB = []
         currentBatchNA = []
         currentBatchNB = []
+        # current batch list (UV)
+        currentBatchAuv = []
+        currentBatchBuv = []
+        currentBatchNAuv = []
+        currentBatchNBuv = []
         # matchA / matchB are extract from keypoints at a specific batch
         for i in range(batchIndexKeyoints.shape[0]):
             if batchIndexKeyoints[i] == batch:
+                # update UV
+                currentBatchAuv.append((int(kp_A[i,0]), int(kp_A[i,1])))
+                currentBatchBuv.append((int(kp_B[i,0]), int(kp_B[i,1])))
+                # update Linear
                 currentBatchA.append(W * int(kp_A[i,1]) + int(kp_A[i,0]))
                 currentBatchB.append(W * int(kp_B[i,1]) + int(kp_B[i,0]))
-            else:
-                continue
         # update global match list
         matchA.append(currentBatchA)
         matchB.append(currentBatchB)
@@ -116,16 +126,25 @@ def CorrespondenceGenerator(Matcher, ImgA, ImgB, NumberNonMatchPerMatch, device)
         for i in range(len(currentBatchA)):
             sample = 0
             while (sample != NumberNonMatchPerMatch):
-                rdUVW = random.randint(0, (W*H)-1)
-                if rdUVW != currentBatchB[i]:
+                # Sample from the entire image
+                randNumber = torch.rand(2,1)
+                randNumber[0,:] = randNumber[0,:]*W
+                randNumber[1,:] = randNumber[1,:]*H
+                randNumberInt = torch.floor(randNumber)
+                rdUVW = (randNumberInt[0], randNumberInt[1])
+                if rdUVW != currentBatchBuv[i] and np.absolute(rdUVW[0]-currentBatchAuv[i][0]) > nonMatchTh  and np.absolute(rdUVW[1]-currentBatchAuv[i][1]) > nonMatchTh:
+                    # update UV
+                    currentBatchNAuv.append(currentBatchAuv[i])
+                    currentBatchNBuv.append(rdUVW)
+                    # update Linear
                     currentBatchNA.append(currentBatchA[i])
-                    currentBatchNB.append(rdUVW)
+                    currentBatchNB.append(W * int(rdUVW[1]) + int(rdUVW[0]))
                     sample += 1
         # update global non-match list
         nonMatchA.append(currentBatchNA)
         nonMatchB.append(currentBatchNB)
     # return the batched match/non-match
-    return matchA, matchB, nonMatchA, nonMatchB
+    return matchA, matchB, nonMatchA, nonMatchB, kp_A, kp_B
 
 # Set the training/inference device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -140,7 +159,7 @@ imgAFolderTraining = "/home/neurotronics/Bureau/DDN/dataset/ImgA"
 imgBFolderTraining = "/home/neurotronics/Bureau/DDN/dataset/ImgB"
 trainingDataset = ImagePairDataset(ImgADir=imgAFolderTraining, ImgBDir=imgBFolderTraining, Augmentation=False)
 # Init dataloader for training and testing
-batchSize = 3
+batchSize = 1
 trainingLoader = data.DataLoader(trainingDataset, batch_size=batchSize, shuffle=False, num_workers=4)
 print("Dataset loaded !")
 
@@ -158,25 +177,9 @@ def imshow(inp, title, tel):
     plt.pause(0.001)  # pause a bit so that plots are updated
 
 for data in trainingLoader:
-    out1c = torchvision.utils.make_grid(data['image A Match'])
-    out2c = torchvision.utils.make_grid(data['image B Match'])
-    out1 = torchvision.utils.make_grid(data['image A'])
-    out2 = torchvision.utils.make_grid(data['image B'])
-    imshow(out1, title="batch A", tel=True)
-    plt.show()
-    imshow(out2, title="batch B", tel=True)
-    plt.show()
-    imshow(out1c, title="batch AC", tel=False)
-    plt.show()
-    imshow(out2c, title="batch BC", tel=False)
-    plt.show()
     inputBatchACorr = data['image A Match']
-    print(inputBatchACorr.size())
     inputBatchBCorr = data['image B Match']
-    inputBatchA = data['image A']
-    inputBatchB = data['image B']
-    matchA, matchB, nonMatchA, nonMatchB = CorrespondenceGenerator(Matcher=matcher,
-                                                                   ImgA=inputBatchACorr,
-                                                                   ImgB=inputBatchBCorr,
-                                                                   NumberNonMatchPerMatch=150,
-                                                                   device=device)
+    matchA, matchB, nonMatchA, nonMatchB, mkpts0, mkpts1 = CorrespondenceGenerator(Matcher=matcher,
+                                                                   ImgA=inputBatchACorr.to(device),
+                                                                   ImgB=inputBatchBCorr.to(device),
+                                                                   NumberNonMatchPerMatch=150)
